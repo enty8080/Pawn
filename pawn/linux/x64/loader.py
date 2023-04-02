672ef1e0ff0620d7bb9058ea5c85257409a06597
Ivan Nikolsky <enty8080@gmail.com>
2023-04-02 16:21:50 +0400

    Update loader.py

--- pawn/linux/x64/loader.py
+++ pawn/linux/x64/loader.py
@@ -2,6 +2,7 @@
 MIT License
 
 Copyright (c) 2020-2023 EntySec
+Copyright (c) 2023 Tomas Globis
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
